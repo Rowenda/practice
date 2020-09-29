9196c8bc0fa2608c2169725808f85780085d08df
Rowenda <badbat_69@hotmail.com>
2020-09-29 14:15:09 +0200

    first commit

---
+++ avatarClass.php
@@ -0,0 +1,34 @@
+<?php
+ require "vendor/autoload.php";
+
+ class  Avatar{
+
+    public $avatar = [];
+     
+     /* $size = nombre de cases du tableau  $colors = les couleurs voulues par l'utilisateur*/
+    public function construct__(int $size, array $colors){
+        $midle= ($size/2);
+
+            for ($x= 0; $x< $size ; $x++) {
+                $avatar [] = [];
+
+                for ($y=0; $y < $size ; $y++){
+            
+                    if ($y < $midle) {
+                    $randcolors = array_rand( array_flip($colors), 1 );
+                    $avatar[$x][$y] = $randcolors;
+                    }
+                    else {
+                    $avatar[$x][$y] = $avatar [$x][$size-$y-1];
+                    }
+                }
+            }
+    return $avatar;
+    }
+
+    public function display() {
+        
+        dump ($this->avatar);
+    }
+
+}
